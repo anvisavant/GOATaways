11ea867d4a332718cbd0e4065420f530869aa088
Anvi Savant <as3568@cornell.edu>
2026-03-20 20:32:11 -0400

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -65,5 +65,6 @@ def health():
     return jsonify({"status": "ok"})
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port = 5001)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
